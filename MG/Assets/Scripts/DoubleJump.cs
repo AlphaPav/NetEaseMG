afb842355b94c3a3403c43d4afaf7570289d12f9
AlphaPav <912960502@qq.com>
2018-08-25 19:00:11 +0800

    0.0

---
+++ MG/Assets/Scripts/DoubleJump.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour {
+
+
+
+	float force = 250;
+	bool isJump = false;
+	bool isDoubleJump = false;
+	int JumpNum = 0;
+	Rigidbody rb;
+	void Start()
+	{
+		rb= GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			JumpNum++;
+			if (JumpNum > 2) return;
+			if (JumpNum == 1)
+			{
+
+				rb.AddForce(Vector3.up * force);
+			}
+			else if (JumpNum == 2)
+			{
+
+				rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+				rb.AddForce(Vector3.up * force);
+			}
+		}
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.name == "Quad")//碰撞的是quad  
+		{
+
+			JumpNum = 0;
+		}
+	}
+}
---
+++ MG/Assets/Scripts/FollowHero.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHero : MonoBehaviour {
+	public GameObject[] follow;
+
+	GameObject boss;
+	public float xspeed = 1f;//move force
+	public float jumpSpeed = 500f;//jump force
+	Rigidbody rb;
+
+	Vector3 lastPos;
+	const int Pause = 10;
+	int Delay = Pause;
+
+	public Vector3[][] offset;
+
+	void Start()
+	{
+		/*follow[0] = GameObject.Find("cube1");
+		follow[1] = GameObject.Find("cube2");
+		follow[2] = GameObject.Find("cube3");
+		follow[3] = GameObject.Find("cube5");*/
+
+		offset = new Vector3[10][];
+		for (int i = 0; i < 6; i++)
+		{
+			offset[i] = new Vector3[Pause];
+			for (int j = 0; j < Pause; j++)
+			{
+				offset[i][j] = new Vector3(0, 0, 0);
+			}
+		}
+
+		rb = GetComponent<Rigidbody>();
+		lastPos = transform.position;
+	}
+	void Update()
+	{
+		Move();
+
+		if (Delay == 0)
+		{
+			for (int i = 5; i > 0; i--)
+			{
+				for (int j = 0; j < Pause; j++)
+				{
+					offset[i][j] = offset[i - 1][j];
+				}
+			}
+			offset[0] = new Vector3[10];
+			for (int j = 0; j < Pause; j++)
+			{
+				offset[0][j] = new Vector3(0, 0, 0);
+			}
+			Delay = Pause;
+		}
+
+		for (int i = 1; i < 6; i++)
+		{
+			follow[i].transform.Translate(offset[i][Pause-Delay]);
+		}
+
+		offset[0][Pause-Delay] = transform.position - lastPos;
+		lastPos = transform.position;
+
+		//Debug.Log(offset);
+
+
+
+		Delay--;
+
+	}
+
+	void Move()
+	{
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(transform.right * xspeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(-1 * transform.right * xspeed * Time.deltaTime);
+		}
+		/*if (Input.GetKey(KeyCode.Space))
+		{
+			Vector3 force = new Vector3(0, 1, 0);
+			rb.AddForce(force * jumpSpeed);
+		}*/
+	}
+}
+
+
