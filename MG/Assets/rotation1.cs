2a11d0356f826c591ea2f128ffd00ed69057d6ae
wjn956624079 <42687016+wjn956624079@users.noreply.github.com>
2018-08-25 22:37:02 +0800

    123

---
+++ MG/Assets/rotation1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation1 : MonoBehaviour {
+
+	private Transform tr1;
+	// Use this for initialization
+	void Start () {
+		tr1 = gameObject.GetComponent<Transform> ();
+	}
+//	Vector3 v1;
+//	v1=new Vector3(0,0,2f);
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Q)) 
+		{
+			tr1.Rotate (Vector3.forward, 0.1f);
+		}
+		if (Input.GetKey (KeyCode.E)) 
+		{
+			tr1.Rotate (Vector3.forward, -0.1f);
+		}
+	}
+}
