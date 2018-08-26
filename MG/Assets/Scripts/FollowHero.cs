f4b1864627cf5b95e38aa978743b360744a049bd
ItemZheng <3160102332@zju.edu.cn>
2018-08-26 11:11:00 +0800

    Followers will be generated dymically

--- MG/Assets/Scripts/FollowHero.cs
+++ MG/Assets/Scripts/FollowHero.cs
@@ -18,22 +18,29 @@ public class Global{
 
     //抗药性
     public static int HeroResistance = 0;  // 主角抗药性 0-100 百分比免伤
+
+    //生产孩子的位置半径
+    public static float ChildRadius = 0.7f;
+
 }
 
 public class FollowHero : MonoBehaviour {
     // 跟随的游戏物品
-    public GameObject[] follow;
-
-	GameObject boss;
+    private GameObject[] follow;
+    
+    //运动相关速度
 	public float xspeed = 2f;//move force
 	public float jumpSpeed = 700f;//jump force
 	Rigidbody rb;
 
     //上次位置
 	Vector3 lastPos;
-	const int Pause = 10;
+
+    //延时10帧
+    const int Pause = 10;
 	int Delay = Pause;
 
+    //保存每个物体的接下来的运动
 	public Vector3[][] offset;
 
 	void Start()
@@ -42,8 +49,10 @@ public class FollowHero : MonoBehaviour {
 		offset = new Vector3[Global.MaxHeroBlood][];
 
         //初始化变量
+        follow = new GameObject[Global.MaxHeroBlood];
 		for (int i = 0; i < Global.MaxHeroBlood; i++)
 		{
+            follow[i] = null;
 			offset[i] = new Vector3[Pause];
 
 			for (int j = 0; j < Pause; j++)
@@ -57,12 +66,13 @@ public class FollowHero : MonoBehaviour {
 		lastPos = transform.position;
 
         //设置全局变量
-        Global.HeroBlood = 1;
+        Global.HeroBlood = 8;
         Global.HeroType = Global.A;
     }
 	void Update()
 	{
 		Move();
+        SetFollowSubHero();
         
         //延时10帧, 开始更新游戏对象的位置
 		if (Delay == 0)
@@ -92,13 +102,56 @@ public class FollowHero : MonoBehaviour {
 		offset[0][Pause-Delay] = transform.position - lastPos;
 		lastPos = transform.position;
 
-
+        //延时--
 		Delay--;
 
 	}
 
-    public void set(){
+    public void SetFollowSubHero(){
+        for (int i = 1; i < Global.HeroBlood; i++)
+        {
+            if(follow[i] == null)
+            {
+                //新建物体
+                GameObject follower = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                
+                //生成孩子的大小，随机大小  0.45 -- 0.65
+                Vector3 MyScale = transform.lossyScale * Random.Range(0.3f, 0.8f);
+                follower.transform.localScale = MyScale;
+                
+                //生成孩子的位置
+                Vector3 MyOffset = new Vector3(Random.Range(0.6f, 1.7f), 0, Random.Range(0.6f, 1.7f));
+                
+                //决定是 1 or -1
+                float flagX = Random.Range(0f, 1f);
+                float flagZ = Random.Range(0f, 1f);
+                //Debug.Log(flagX);
+                flagX = flagX > 0.5 ? 1 : -1;
+                flagZ = flagZ > 0.5 ? 1 : -1;
 
+                //判断是否是
+                MyOffset = new Vector3(flagX * MyOffset[0], MyOffset[1], MyOffset[2] * flagZ);
+                MyOffset = transform.position + MyOffset * Global.ChildRadius;
+
+                MyOffset = MyOffset - new Vector3(0, transform.lossyScale[1] - follower.transform.lossyScale[1], 0);
+                follower.transform.position = MyOffset;
+
+
+                follower.GetComponent<CapsuleCollider>().isTrigger = true;
+                follow[i] = follower;
+            }
+        }
+
+        //删除多余的孩子
+        for(int i = Global.HeroBlood; i < Global.MaxHeroBlood; i++)
+        {
+            if(follow[i] != null)
+            {
+                Destroy(follow[i]);
+                follow[i] = null;
+            }
+        }
+            
     }
 
 
