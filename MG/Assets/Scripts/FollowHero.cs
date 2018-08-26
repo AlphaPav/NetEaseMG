c7a6651af814253bb2824de3f7e646b764014b1e
ItemZheng <3160102332@zju.edu.cn>
2018-08-26 09:53:53 +0800

    update

--- MG/Assets/Scripts/FollowHero.cs
+++ MG/Assets/Scripts/FollowHero.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Global{
+    //主角类型
+    public const int A = 0;
+    public const int B = 1;
+    public const int C = 2;
+
+    //最大血量
+    public const int MaxHeroBlood = 15;
+
+    //主角变量
+    public static int HeroBlood = 1;      	// 主角血量  决定后面的跟班个数
+
+    public static int HeroType = A;	  	// 主角类型  A or B or C
+
+    //抗药性
+    public static int HeroResistance = 0;  // 主角抗药性 0-100 百分比免伤
+}
+
 public class FollowHero : MonoBehaviour {
-	public GameObject[] follow;
+    // 跟随的游戏物品
+    public GameObject[] follow;
 
 	GameObject boss;
 	public float xspeed = 2f;//move force
 	public float jumpSpeed = 700f;//jump force
 	Rigidbody rb;
 
+    //上次位置
 	Vector3 lastPos;
 	const int Pause = 10;
 	int Delay = Pause;
@@ -18,27 +38,36 @@ public class FollowHero : MonoBehaviour {
 
 	void Start()
 	{
+        //每个物体的连续每10帧动画 
+		offset = new Vector3[Global.MaxHeroBlood][];
 
-		offset = new Vector3[10][];
-		for (int i = 0; i < 6; i++)
+        //初始化变量
+		for (int i = 0; i < Global.MaxHeroBlood; i++)
 		{
 			offset[i] = new Vector3[Pause];
+
 			for (int j = 0; j < Pause; j++)
 			{
 				offset[i][j] = new Vector3(0, 0, 0);
 			}
 		}
 
+        //获得刚体组件
 		rb = GetComponent<Rigidbody>();
 		lastPos = transform.position;
-	}
+
+        //设置全局变量
+        Global.HeroBlood = 1;
+        Global.HeroType = Global.A;
+    }
 	void Update()
 	{
 		Move();
-
+        
+        //延时10帧, 开始更新游戏对象的位置
 		if (Delay == 0)
 		{
-			for (int i = 5; i > 0; i--)
+			for (int i = Global.HeroBlood - 1; i > 0; i--)
 			{
 				for (int j = 0; j < Pause; j++)
 				{
@@ -53,37 +82,37 @@ public class FollowHero : MonoBehaviour {
 			Delay = Pause;
 		}
 
-		for (int i = 1; i < 6; i++)
+        //开始移动
+		for (int i = 1; i < Global.HeroBlood; i++)
 		{
 			follow[i].transform.Translate(offset[i][Pause-Delay]);
 		}
 
+        // 更新最新的游戏位置
 		offset[0][Pause-Delay] = transform.position - lastPos;
 		lastPos = transform.position;
-
-		//Debug.Log(offset);
-
 
 
 		Delay--;
 
 	}
 
+    public void set(){
+
+    }
+
+
 	void Move()
 	{
+        //AD移动
 		if (Input.GetKey(KeyCode.D))
 		{
-			transform.Translate(transform.right * xspeed * Time.deltaTime);
+			transform.Translate(new Vector3(1,0,0) * xspeed * Time.deltaTime, Space.World);
 		}
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Translate(-1 * transform.right * xspeed * Time.deltaTime);
+			transform.Translate(-1 * new Vector3(1, 0, 0) * xspeed * Time.deltaTime, Space.World);
 		}
-		/*if (Input.GetKey(KeyCode.Space))
-		{
-			Vector3 force = new Vector3(0, 1, 0);
-			rb.AddForce(force * jumpSpeed);
-		}*/
 	}
 }
 
