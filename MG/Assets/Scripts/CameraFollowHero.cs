4d4d860b9cb6b659a250fe489cafe8e1265a0712
AlphaPav <912960502@qq.com>
2018-08-26 14:02:01 +0800

    G1

--- MG/Assets/Scripts/CameraFollowHero.cs
+++ MG/Assets/Scripts/CameraFollowHero.cs
@@ -7,21 +7,25 @@ public class CameraFollowHero : MonoBehaviour {
 	Vector3 offset;
 	Vector3 beginOffset;
 	// Use this for initialization
+
+	Vector3 target;
 	void Start () {
 		hero = GameObject.FindWithTag("Hero");
 		offset = transform.position - hero.transform.position;
 		beginOffset = offset;
-
+		target = transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Global.isCameraFollowHero == true) {
-			Vector3 delte = offset + hero.transform.position - transform.position;
-			transform.Translate (delte, Space.World);
+		target = offset + hero.transform.position;
 
+		if (Global.isCameraFollowHero == true) {
+			if(Vector3.Distance(transform.position, target) > 0.05)
+			{
+				transform.Translate((target - transform.position).normalized * 3.0f * Time.deltaTime);
+			}
 		}
 
-	
 	}
 }
