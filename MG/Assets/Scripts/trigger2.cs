0c29a2a353474dd1638ef448d82e0e7542670dfe
AlphaPav <912960502@qq.com>
2018-08-26 16:00:23 +0800

    hide and appear trigger

--- MG/Assets/Scripts/trigger2.cs
+++ MG/Assets/Scripts/trigger2.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class trigger2 : MonoBehaviour {
+	GameObject cube;
+	GameObject myhero;
+	Material _mat;
+
+	float _alpha = 1;
+	bool _isFadeIn = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		cube= GameObject.FindWithTag("01Move2");
+		myhero= GameObject.FindWithTag("Hero");
+		_mat = cube.GetComponent<Renderer>().material;
+		_mat.color = new Color(1, 0, 0, _alpha);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (_isFadeIn) {
+			if (_alpha > 0) {
+				_alpha -= Time.deltaTime / 2;
+				_mat.color = new Color(0, 0, 1, _alpha);
+			} else {
+				_isFadeIn = false;
+			}
+		} 
+
+
 	}
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Hero")//碰撞的是quad  
+		{
+				_isFadeIn = true;
+		    	_alpha=  _mat.color.a;
+		}
+	}
+
 }
---
+++ MG/Assets/Scripts/trigger3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger3 : MonoBehaviour {
+	bool isused =false;
+	GameObject cube;
+	// Use this for initialization
+	void Start () {
+		cube = this.gameObject;
+		if (cube == null)
+			Debug.Log ("cube=null");
+		Debug.Log (Global.HeroResistance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.tag == "Hero")
+		{
+			
+			if (isused == false) {
+				Global.HeroResistance++;
+				Debug.Log (Global.HeroResistance);
+				isused = true;
+				cube.active = false;
+			}
+
+		}
+	}
+
+}
