1207876f168b99d4114d11ba43bafca998124574
wjn956624079 <42687016+wjn956624079@users.noreply.github.com>
2018-08-27 20:57:24 +0800

    123

---
+++ MG/Assets/wow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wow : MonoBehaviour {
+
+	private AudioSource m1;
+	// Use this for initialization
+	void Start()
+	{
+		m1 = gameObject.GetComponent<AudioSource>();
+	}
+	int z;
+	int flag=1;
+	void Update()
+	{
+
+		if (Input.GetKeyDown (KeyCode.X)) 
+		{
+			m1.Play();
+		}
+
+	}
+
+}
---
+++ MG/Assets/wow1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wow1 : MonoBehaviour {
+
+	private AudioSource m1;
+	// Use this for initialization
+	void Start()
+	{
+		m1 = gameObject.GetComponent<AudioSource>();
+	}
+	int z=0;
+	int flag=1;
+	void Update()
+	{
+		
+		if (Input.GetKeyDown (KeyCode.X)) 
+		{
+			m1.Play();
+		}
+
+	}
+
+
+
+}
